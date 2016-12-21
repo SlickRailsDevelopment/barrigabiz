9cc37ed634dcc570c2d31b7ce2901e7f0d3e1893
slick <slick@slick-tabi.(none)>
2016-12-21 11:57:25 -0600

    Redirects hompage

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
   resources :pages
 
-  #get '/home' => 'pages#home'
-  #get '/bio' => 'pages#bio'
-  #get '/services' => 'pages#services'
-  #get '/contact' => 'pages#contact'
-  #get '/videos' => 'pages#videos'
-  get '/error' => 'pages#error'
+  get '/home' => 'pages#home'
+  get '/bio' => 'pages#bio'
+  get '/services' => 'pages#services'
+  get '/contact' => 'pages#contact'
+  get '/videos' => 'pages#videos'
+  #get '/error' => 'pages#error'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'pages#error'
+  root 'pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
